5cd9df7f134a2cf24eb3bffed76715ef1df5ba47
Joshua Spears <j.dspears@yahoo.com>
2019-12-13 15:23:09 -0800

    blah

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const sequelize = require('sequelize')
 const logger = require('morgan')
 
 // CONTROLLERS
@@ -19,13 +18,6 @@ const routes = require('./app_api/routes/indexRoutes')
 
 const app = express()
 
-const tripData = require('./app_api/data/trip')
-const locationData = require('./app_api/data/location')
-const trailData = require('./app_api/data/trail')
-const campData = require('./app_api/data/campsite')
-const activityData = require('./app_api/data/activity')
-const centerData = require('./app_api/data/visitorCenter')
-
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -33,10 +25,12 @@ app.use(bodyParser.text())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
 app.use(logger('dev'))
 app.use(routes)
+
 if (isDev) {
     app.use(express.static('app_client/public'))
+} else {
+    app.use(express.static('app_client/build'))
 }
-app.use(express.static('app_client/build'))
 
 
 models.sequelize.sync({ force: isDev }).then(function () {
