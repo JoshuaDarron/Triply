d0ff4b7a2d1ea1befa6feebbe737ea03e23536d1
Joshua Spears <j.dspears@yahoo.com>
2019-12-13 15:01:44 -0800

    blah

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-require("dotenv").config();
+require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -19,24 +19,24 @@ const routes = require('./app_api/routes/indexRoutes')
 
 const app = express()
 
-const tripData = require("./app_api/data/trip")
-const locationData = require("./app_api/data/location")
-const trailData = require("./app_api/data/trail")
-const campData = require("./app_api/data/campsite")
-const activityData = require("./app_api/data/activity")
-const centerData = require("./app_api/data/visitorCenter");
+const tripData = require('./app_api/data/trip')
+const locationData = require('./app_api/data/location')
+const trailData = require('./app_api/data/trail')
+const campData = require('./app_api/data/campsite')
+const activityData = require('./app_api/data/activity')
+const centerData = require('./app_api/data/visitorCenter')
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(logger('dev'));
-app.use(routes);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.text())
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+app.use(logger('dev'))
+app.use(routes)
 if (isDev) {
-    app.use(express.static('app_client/public'));
+    app.use(express.static('app_client/public'))
 }
-app.use(express.static('app_client/build'));
+app.use(express.static('app_client/build'))
 
 
 models.sequelize.sync({ force: isDev }).then(function () {
@@ -45,32 +45,32 @@ models.sequelize.sync({ force: isDev }).then(function () {
         authCtrl._generateSalt(), 
         authCtrl._generateSalt(), 
         authCtrl._generateSalt()
-    ];
+    ]
 
     if (isDev) {
         models.User.bulkCreate([
             {
-                email: "josh@spears.com",
+                email: 'josh@spears.com',
                 salt: saltArr[0],
-                hash: authCtrl._generateHash("joshspears", saltArr[0])
+                hash: authCtrl._generateHash('joshspears', saltArr[0])
             },
             {
-                email: "jason@daniel.com",
+                email: 'jason@daniel.com',
                 salt: saltArr[1],
-                hash: authCtrl._generateHash("jasondaniel", saltArr[1])
+                hash: authCtrl._generateHash('jasondaniel', saltArr[1])
             },
             {
-                email: "melodie@chi.com",
+                email: 'melodie@chi.com',
                 salt: saltArr[2],
-                hash: authCtrl._generateHash("melodiechi", saltArr[2])
+                hash: authCtrl._generateHash('melodiechi', saltArr[2])
             },
             {
-                email: "rey@adam.com",
+                email: 'rey@adam.com',
                 salt: saltArr[3],
-                hash: authCtrl._generateHash("reyadamcruz", saltArr[3])
+                hash: authCtrl._generateHash('reyadamcruz', saltArr[3])
             }
         ])
     } // END IF 
 
-    app.listen(PORT, () => console.log(`\nApp listening on PORT: ${PORT}`)); // END LISTEN
-});
+    app.listen(PORT, () => console.log(`\nApp listening on PORT: ${PORT}`))
+})
