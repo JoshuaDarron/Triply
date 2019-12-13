4b9950565e801ff59380dc80c55cb96b52120bf6
Joshua Spears <j.dspears@yahoo.com>
2019-12-13 14:21:21 -0800

    Synax

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
 
 require("dotenv").config();
 
-/*  
-    =====================================================================================
-    DEPENDENCIES / VARIABLES
-    ===================================================================================== 
-*/
-
 const express = require('express')
-    , bodyParser = require('body-parser')
-    , sequelize = require('sequelize')
-    , logger = require('morgan')
+const bodyParser = require('body-parser')
+const sequelize = require('sequelize')
+const logger = require('morgan')
 
-    // CONTROLLERS
-    , authCtrl = require('./app_api/controllers/auth/authCtrl')
-    // DB
-    , models = require('./app_api/models')
+// CONTROLLERS
+const authCtrl = require('./app_api/controllers/auth/authCtrl')
+//DB
+const models = require('./app_api/models')
 
-    , PORT = process.env.PORT || 3001
-    // ENVIRONMENT
-    , isDev = process.env.NODE_ENV === 'development'
-    // ROUTES
-    , routes = require('./app_api/routes/indexRoutes')
+const PORT = process.env.PORT || 3001
+// ENVIRONMENT
+const isDev = process.env.NODE_ENV === 'development'
+// ROUTES
+const routes = require('./app_api/routes/indexRoutes')
 
-    , app = express()
+const app = express()
 
-    , tripData = require("./app_api/data/trip")
-    , locationData = require("./app_api/data/location")
-    , trailData = require("./app_api/data/trail")
-    , campData = require("./app_api/data/campsite")
-    , activityData = require("./app_api/data/activity")
-    , centerData = require("./app_api/data/visitorCenter");
+const tripData = require("./app_api/data/trip")
+const locationData = require("./app_api/data/location")
+const trailData = require("./app_api/data/trail")
+const campData = require("./app_api/data/campsite")
+const activityData = require("./app_api/data/activity")
+const centerData = require("./app_api/data/visitorCenter");
 
-/*  
-    =====================================================================================
-    MIDDLEWARE
-    ===================================================================================== 
-*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,43 +38,39 @@ if (isDev) {
 }
 app.use(express.static('app_client/build'));
 
-/*  
-    =====================================================================================
-    SYNC & START SERVERS
-    ===================================================================================== 
-*/
 
 models.sequelize.sync({ force: isDev }).then(function () {
+    const saltArr = [
+        authCtrl._generateSalt(), 
+        authCtrl._generateSalt(), 
+        authCtrl._generateSalt(), 
+        authCtrl._generateSalt()
+    ];
 
-    // const saltArr = [authCtrl._generateSalt(), authCtrl._generateSalt(), authCtrl._generateSalt(), authCtrl._generateSalt()];
-    // if (isDev) {
-
-
-    //     models.User.bulkCreate([
-    //         {
-    //             email: "josh@spears.com",
-    //             salt: saltArr[0],
-    //             hash: authCtrl._generateHash("joshspears", saltArr[0])
-    //         },
-    //         {
-    //             email: "jason@daniel.com",
-    //             salt: saltArr[1],
-    //             hash: authCtrl._generateHash("jasondaniel", saltArr[1])
-    //         },
-    //         {
-    //             email: "melodie@chi.com",
-    //             salt: saltArr[2],
-    //             hash: authCtrl._generateHash("melodiechi", saltArr[2])
-    //         },
-    //         {
-    //             email: "rey@adam.com",
-    //             salt: saltArr[3],
-    //             hash: authCtrl._generateHash("reyadamcruz", saltArr[3])
-    //         }
-    //     ])
-    // } // END IF 
-
+    if (isDev) {
+        models.User.bulkCreate([
+            {
+                email: "josh@spears.com",
+                salt: saltArr[0],
+                hash: authCtrl._generateHash("joshspears", saltArr[0])
+            },
+            {
+                email: "jason@daniel.com",
+                salt: saltArr[1],
+                hash: authCtrl._generateHash("jasondaniel", saltArr[1])
+            },
+            {
+                email: "melodie@chi.com",
+                salt: saltArr[2],
+                hash: authCtrl._generateHash("melodiechi", saltArr[2])
+            },
+            {
+                email: "rey@adam.com",
+                salt: saltArr[3],
+                hash: authCtrl._generateHash("reyadamcruz", saltArr[3])
+            }
+        ])
+    } // END IF 
 
     app.listen(PORT, () => console.log(`\nApp listening on PORT: ${PORT}`)); // END LISTEN
-
-}); // END SYNC 
+});
